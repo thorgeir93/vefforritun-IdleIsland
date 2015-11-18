126f3fa8c6d778b103084deaab9a972dd98943a1
Mani <wowthorkell@gmail.com>
2015-11-18 16:57:30 +0000

    The game stuff is now in the public directory

---
+++ public/game/DBconnector.js
@@ -0,0 +1,12 @@
+function DBconnector(){
+
+	console.log('inn i dbconnector constr');
+}
+
+DBconnector.prototype.send = function(userData){
+	//implement
+}
+
+DBconnector.prototype.receiver = function(userName){
+	//implement
+}
